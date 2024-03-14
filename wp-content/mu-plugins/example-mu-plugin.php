8d6b10a80ce919a2cccd3cf4722c02dcc4fd6df3
David Smith <david.smith@pagely.com>
2024-03-14 15:13:13 -0500

    Update example mu-plugin comments

---
+++ wp-content/mu-plugins/example-mu-plugin.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ * Plugin Name: Totally Cool MU-Plugin
+ * Version: 0.42.0
+ *
+ * This mu-plugin doesn't do anything (obviously). It's here as an example
+ * of how you could add an mu-plugin to your repo. The key thing to keep
+ * in mind is getting your top-level .gitignore file right - you'll need
+ * to ignore /wp-content/mu-plugins/pagely* (to avoid overwriting any of
+ * Pagely's own mu-plugins), but otherwise this directory is as safe as
+ * any other.
+ *
+ */
